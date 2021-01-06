012513d825a2cfadc3167be34845372a3d1075a6
Ricardo Plascencia Silva <ricardo072@gmail.com>
2021-01-06 14:29:45 -0600

    adding a simple functional component

--- ReactCG2021/react-complete-guide/src/App.js
+++ ReactCG2021/react-complete-guide/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   // react will call this method to render something to the DOM.
   render() {
 
-    // we are returning JSX, not HTML.
+    // we are returning JSX, it seems to be HTML, but is NOT.
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
+        <Person />
       </div>
     );
     
---
+++ ReactCG2021/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Capital letter on the file name is just a React convention.
+
+// There are a few ways to create a new Component. A new 
+// component could be just a simple JS funcion, in its siplest 
+// form, it's just a function that returns some JSX. You could 
+// also use the ES6 syntax of creating a variable which holds a 
+// function, which in the end would result in the same.
+// As a best practice, we will use ES6:
+
+const person = () => {
+    return <p>I'm a Person</p>
+}
+
+export default person;
