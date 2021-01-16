278249332c90786240f640214f21c69ad60d0c2a
Ricardo Plascencia Silva <ricardo072@gmail.com>
2021-01-16 14:09:19 -0600

    A better project structure

--- ReactCG2021/react-complete-guide/src/containers/App.js
+++ ReactCG2021/react-complete-guide/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   // we should use 'state' if we need to manage some 
