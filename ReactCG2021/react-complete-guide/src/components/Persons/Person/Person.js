fe9afce421576fa3de641ab51f717dc7015ededd
Ricardo Plascencia Silva <ricardo072@gmail.com>
2021-01-17 01:27:52 -0600

    Using React.Fragment instead of Aux

--- ReactCG2021/react-complete-guide/src/components/Persons/Person/Person.js
+++ ReactCG2021/react-complete-guide/src/components/Persons/Person/Person.js
@@ -27,13 +27,13 @@ class Person extends Component {
 
         return (
             // we can use this new StyleDiv as a regular react component:
-            <Aux>
+            <React.Fragment>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age} years old
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </React.Fragment>
         );
     }
 }
--- ReactCG2021/react-complete-guide/src/hoc/Aux.js
+++ ReactCG2021/react-complete-guide/src/hoc/Aux.js
@@ -1,4 +1,6 @@
 // High order component that is an empty wrapper
+// It does exactly the same as React.Fragment
+// technically they are the same
 const aux = props => props.children;
 
 export default aux;
