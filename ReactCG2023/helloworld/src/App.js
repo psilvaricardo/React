e145deb8c3dbeb5b3566e56c13d22dd38c81c327
Ricardo Plascencia Silva <ricardo072@gmail.com>
2023-09-16 17:44:03 -0500

    Adding basic custom component

--- ReactCG2023/helloworld/src/App.js
+++ ReactCG2023/helloworld/src/App.js
@@ -1,3 +1,4 @@
+import FirstGenericComponent from './components/FirstGenericComponent';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,15 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload...
+          <FirstGenericComponent></FirstGenericComponent>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ ReactCG2023/helloworld/src/components/FirstGenericComponent.js
@@ -0,0 +1,16 @@
+
+// Ctrl + Shift + I : fill format the code...
+
+function FirstGenericComponent() {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <div>Title</div>
+                <div>Amount</div>
+            </div>
+        </div>
+    );
+}
+
+export default FirstGenericComponent;
