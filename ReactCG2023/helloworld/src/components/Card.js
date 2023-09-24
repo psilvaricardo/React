42c7ad0dda7ffe47c9c7aaf191ac5edc0532dab5
Ricardo Plascencia Silva <ricardo072@gmail.com>
2023-09-24 09:09:05 -0500

    Adding code documentation for Composition

--- ReactCG2023/helloworld/src/components/Card.js
+++ ReactCG2023/helloworld/src/components/Card.js
@@ -1,12 +1,14 @@
 import './Card.css';
 
 function Card(props) {
-    // anything we receive as a class from the outside is added here.
+    // anything we receive as a css class from the outside is added here.
     const classes = 'card ' + props.className;
 
-    // this will have the common logic to style the 'container' rounded corners
-    // this is some kind of wrapper around components that encapsulates common functionality and CSS
-    // children is a reserved word and always is going to be whatever between the opening and closing tags of your custom component
+    // Composition: This will have the common logic to style the 'so-called container' rounded corners, 
+    //              this is all about reusing code, avoind code duplication and keeping your other components clean.
+
+    // This is some kind of wrapper around components that encapsulates common functionality and CSS
+    // children is a reserved word and always is going to be whatever you put between the opening and closing tags of your custom component
     return (<div className={classes}> {props.children} </div>);
 }
 
