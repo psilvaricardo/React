25abcde65dd885a7987af5d95fd2125875f35e47
Ricardo Plascencia Silva <ricardo072@gmail.com>
2023-09-23 20:41:05 -0500

    Adding dummy props to make component reusable

--- ReactCG2023/helloworld/src/App.js
+++ ReactCG2023/helloworld/src/App.js
@@ -1,10 +1,27 @@
 import FirstGenericComponent from './components/FirstGenericComponent';
 
 function App() {
+  // adding dummy props...
+  const expenses = [
+    { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14), },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2022, 4, 28) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2023, 9, 21) }
+  ];
+
   return (
     <div>
       <h2>Let's get's started!</h2>
-        <FirstGenericComponent></FirstGenericComponent>
+      {/* Map over expenses and render FirstGenericComponent for each expense */}
+      {expenses.map((expense) => (
+        <FirstGenericComponent
+          key={expense.id} // Don't forget to provide a unique key for each component
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
--- ReactCG2023/helloworld/src/components/FirstGenericComponent.js
+++ ReactCG2023/helloworld/src/components/FirstGenericComponent.js
@@ -3,22 +3,20 @@ import './FirstGenericComponent.css'
 
 // Ctrl + Shift + I : will format the code...
 
-function FirstGenericComponent() {
-    // dummy data for now
-    const expenseDate = new Date(2023,9,23);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 2394.34;
-
-
+function FirstGenericComponent(props) {
+        
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
+            <div>{props.id}</div>
+            <div>{props.key}</div>
         </div>
     );
+
 }
 
 export default FirstGenericComponent;
