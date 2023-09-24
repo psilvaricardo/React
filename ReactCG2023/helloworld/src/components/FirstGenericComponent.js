084ecd2f85c4a5c921d3af1be945b98e265f7264
Ricardo Plascencia Silva <ricardo072@gmail.com>
2023-09-23 21:18:40 -0500

    Adding basic date logic

--- ReactCG2023/helloworld/src/components/FirstGenericComponent.js
+++ ReactCG2023/helloworld/src/components/FirstGenericComponent.js
@@ -9,9 +9,17 @@ function FirstGenericComponent(props) {
     function FirstGenericComponent({date, title, amount})
     */
 
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
