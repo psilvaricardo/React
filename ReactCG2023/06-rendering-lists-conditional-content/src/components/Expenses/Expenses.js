7455c2478e43b9c2e2f09b113802d6553dff6f01
Ricardo Plascencia Silva <ricardo072@gmail.com>
2023-09-30 11:17:19 -0500

    Adding code documentation

--- ReactCG2023/06-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
+++ ReactCG2023/06-rendering-lists-conditional-content/src/components/Expenses/Expenses.js
@@ -20,6 +20,8 @@ const Expenses = (props) => {
             <Card className="expenses">
                 {/* the ExpenseFilter is a "controled component" because the value, the real logic is not handled there, but in a parent component.*/}
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {/* This is already rendering the ExpenseItem in a dynamic way using the below dynamic expression
+                    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */}
                 {props.expenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id} // Don't forget to provide a unique key for each component
