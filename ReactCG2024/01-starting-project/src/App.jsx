c9653221b92f8eeadfd5adeabb3f8d09f14c07a0
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-04-20 14:16:52 -0500

    Using alternative props syntax 3

--- ReactCG2024/01-starting-project/src/App.jsx
+++ ReactCG2024/01-starting-project/src/App.jsx
@@ -38,10 +38,9 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
+            ))}
           </ul>
         </section>
       </main>
