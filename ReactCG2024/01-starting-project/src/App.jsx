33a2935bd9e9cd96e6344bb07aa08b38aaebf63a
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-04-20 13:57:47 -0500

    Making components reusable with Props

--- ReactCG2024/01-starting-project/src/App.jsx
+++ ReactCG2024/01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import dynamicImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -20,12 +21,46 @@ function CustomHeader() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <CustomHeader />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The code UI building block."
+              image={componentsImg} 
+            />
+            <CoreConcept 
+              title="Components" 
+              description="The code UI building block."
+              image={componentsImg} 
+            />
+            <CoreConcept 
+              title="Components" 
+              description="The code UI building block."
+              image={componentsImg} 
+            />
+            <CoreConcept 
+              title="Components" 
+              description="The code UI building block."
+              image={componentsImg} 
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
