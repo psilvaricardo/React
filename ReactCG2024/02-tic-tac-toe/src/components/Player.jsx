e89b2c842797e04c9ce6ffdb44d766c4f87690c8
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-17 17:37:01 -0500

    Improvements to save when the user clicks Enter on the input text

--- ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
+++ ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
@@ -9,15 +9,25 @@ const Player = ({ initialName, symbol }) => {
         // because React is scheduling the update behind the scenes, and this way
         // we can warranty that we are using the latest value for the state.
         setIsEditing( (isEditing) => !isEditing );
-    }
+    };
 
-    const handleChange = (event) => {
+    const handleOnTextChange = (event) => {
         setPlayerName(event.target.value);
-    }
+    };
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        setPlayerName(event.target.value);
+        setIsEditing( (isEditing) => !isEditing );
+      }
+    }; 
 
     let editablePlayerName = isEditing ? 
-                     <input type="text" required value={playerName} onChange={(e) => handleChange(e) } /> : 
-                     <span className="player-name">{playerName}</span> ;
+                      <input type="text" required value={playerName} 
+                        onChange={(e) => handleOnTextChange(e) }
+                        onKeyDown={(e) => handleKeyDown(e) } 
+                      /> : 
+                      <span className="player-name">{playerName}</span> ;
     
     let buttonTxt = isEditing ? 'Save' : 'Edit';
     
