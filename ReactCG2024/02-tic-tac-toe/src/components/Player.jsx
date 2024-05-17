f8447391ba1db34bb4f6175f120f672397f860a1
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-16 22:47:05 -0500

    Following best practices to update state based on previous state

--- ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
+++ ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
@@ -4,11 +4,12 @@ const Player = ({name, symbol}) => {
     const[isEditing, setIsEditing] = useState(false);
 
     const handleClick = () => {
-        setIsEditing( !isEditing );
+        // When updating the state based on the old state we MUST pass a funtion
+        setIsEditing( (isEditing) => !isEditing );
     }
 
     let playerName = isEditing ? 
-                     <input type="text" required /> : 
+                     <input type="text" required value={name} /> : 
                      <span className="player-name">{name}</span> ;
     
     let buttonTxt = isEditing ? 'Save' : 'Edit';
