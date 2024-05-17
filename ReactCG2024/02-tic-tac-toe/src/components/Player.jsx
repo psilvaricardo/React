d5743c0748ead0b1acb9d50a48ad108a4f47bb66
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-16 22:28:56 -0500

    Using React State to make it interactive

--- ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
+++ ReactCG2024/02-tic-tac-toe/src/components/Player.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 
 const Player = ({name, symbol}) => {
+    const[isEditing, setIsEditing] = useState(false);
+
+    const handleClick = () => {
+        setIsEditing( !isEditing );
+    }
+
+    let playerName = isEditing ? 
+                     <input type="text" required /> : 
+                     <span className="player-name">{name}</span> ;
+    
+    let buttonTxt = isEditing ? 'Save' : 'Edit';
+    
     return (
         <>
           <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => handleClick() }> {buttonTxt} </button>
           </li>
         </>
     );
