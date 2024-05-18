6e71e5aa6adfe0413fab6036f7b66a8f08c888b6
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-17 19:11:11 -0500

    Updating the GameBoard component in an immutable way

--- ReactCG2024/02-tic-tac-toe/src/components/GameBoard.jsx
+++ ReactCG2024/02-tic-tac-toe/src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,16 +7,29 @@ const initialGameBoard = [
 ];
 
 const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectedSquare = (rowIndex, colIndex) => {
+        // if your state is an array or an object, we must update the state
+        // in an Immutable way, making a copy and updating the copy.
+        // https://academind.com/tutorials/reference-vs-primitive-values
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map(
+            {gameBoard.map(
                 (row, rowIndex) =>
                     <li key={rowIndex}>
                         <ol>
                             {row.map(
                                 (playerSymbol, symbolIndex) => (
                                     <li key={symbolIndex}>
-                                        <button>{playerSymbol}</button>
+                                        <button onClick={(e) => handleSelectedSquare(rowIndex,symbolIndex)}>{playerSymbol}</button>
                                     </li>
                                 )
                             )}
