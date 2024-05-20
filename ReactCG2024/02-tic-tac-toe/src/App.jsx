0874ab981f7c335ad2c604dd1676942350024fbb
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-19 23:20:13 -0500

    Adding Game Over screen and also checking for a Draw

--- ReactCG2024/02-tic-tac-toe/src/App.jsx
+++ ReactCG2024/02-tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Log from "./components/Log";
 import Player from "./components/Player";
+import GameOver from "./components/GameOver";
 import GameBoard from "./components/GameBoard";
 import { WINNING_COMBINATIONS } from './winning-combinations';
 
@@ -53,6 +54,8 @@ const App = () => {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   const handleSelectSquare = (rowIndex, colIndex) => {
     setGameTurns( prevTurns => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -74,7 +77,7 @@ const App = () => {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
         <GameBoard 
           onSelectSquare={handleSelectSquare} 
           board={gameBoard} 
