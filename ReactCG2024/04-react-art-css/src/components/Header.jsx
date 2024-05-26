59d093fa1786c61ed76e1bfdb1a13ca3e71cdaa3
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-26 15:36:18 -0500

    Adding example of CSS Rules and Modules scoping

--- ReactCG2024/04-react-art-css/src/components/Header.jsx
+++ ReactCG2024/04-react-art-css/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import logo from '../assets/logo.png';
+import classes from "./Header.module.css";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
-  );
+    return (
+        <header>
+            <img src={logo} alt="A canvas" />
+            <h1>ReactArt</h1>
+            <p className={classes.paragraph}>
+                A community of artists and art-lovers.
+            </p>
+        </header>
+    );
 };
 
 export default Header;
