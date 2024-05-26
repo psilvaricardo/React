0573c6c10e1c8c0bcc92006500a5546a93e6eb08
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-05-26 14:45:45 -0500

    Init version

---
+++ ReactCG2024/04-react-art-css/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import AuthInputs from "./components/AuthInputs"
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+};
+
+export default App;
