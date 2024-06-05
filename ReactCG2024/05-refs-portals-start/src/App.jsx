a0e8063742b3edb33968d657e447b3b38f298628
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-04 20:01:56 -0500

    Adding challenge cards to this demo project

--- ReactCG2024/05-refs-portals-start/src/App.jsx
+++ ReactCG2024/05-refs-portals-start/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
 
 const App = () => {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Geting tougt" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
+      </div>
     </>
   );
 };
