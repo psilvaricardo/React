5d9407de479903fa2dfb127901937fed02707c29
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-04 19:20:58 -0500

    First step - Adding two-way-binding

--- ReactCG2024/05-refs-portals-start/src/components/Player.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 const Player = () => {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    setSubmitted(true);
+  };
+
     return (
         <section id="player">
-          <h2>Welcome unknown entity</h2>
+          <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
           <p>
-            <input type="text" />
-            <button>Set Name</button>
+            <input type="text" onChange={(e) => handleChange(e)} value={enteredPlayerName} />
+            <button onClick={(e) => handleClick(e)}>Set Name</button>
           </p>
         </section>
       );
