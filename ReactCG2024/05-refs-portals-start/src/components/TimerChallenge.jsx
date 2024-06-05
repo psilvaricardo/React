548351ff5ea34d03b9b2d3be5ee7be4e3022a574
psilvaricardo <125488497+psilvaricardo@users.noreply.github.com>
2024-06-04 20:47:36 -0500

    Using Refs across different functions

--- ReactCG2024/05-refs-portals-start/src/components/TimerChallenge.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/TimerChallenge.jsx
@@ -1,20 +1,38 @@
+import { useState, useRef } from "react";
 
 const TimerChallenge = ({title, targetTime}) => {
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    const handleStart = () => {
+        timer.current = setTimeout( () => {
+            setTimerExpired(true);
+        }, targetTime * 1000 );
+        
+        setTimerStarted(true);
+    };
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+    };
+
     return (
-    <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-            {targetTime} second{targetTime>1 ? 's': ''}
-        </p>
-        <p>
-            <button>
-                Start Challenge
-            </button>
-        </p>
-        <p className="">
-            Time is running... / Timer inactive.
-        </p>
-    </section>
+        <section className="challenge">
+            <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
+            <p className="challenge-time">
+                {targetTime} second{targetTime>1 ? 's': ''}
+            </p>
+            <p>
+                <button onClick={timerStarted ? (e) => handleStop() : (e) => handleStart()}>
+                    {timerStarted ? 'Stop' :  'Start'} Challenge
+                </button>
+            </p>
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
+            </p>
+        </section>
     );
 };
 
