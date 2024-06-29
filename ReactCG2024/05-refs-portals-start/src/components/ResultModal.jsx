642e0cd8218b95c599d286248f97e16abee48354
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-29 13:29:54 -0500

    Fixing dialog's ref param

--- ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 
 const ResultModal = forwardRef(({ result, targetTime }, ref) => {
     return (
-        <dialog className="result-modal">
+        <dialog className="result-modal" ref={ref}>
             <h2>You {result} </h2>
             <p>
                 The target time was{" "}
