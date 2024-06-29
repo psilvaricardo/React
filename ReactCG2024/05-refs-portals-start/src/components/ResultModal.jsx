ad606fabb75d6e7d20dea11188573e4eee314a76
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-29 14:50:45 -0500

    Enhancing the modal and timer components

--- ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
@@ -1,39 +1,45 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(({ result, targetTime }, ref) => {
-    const dialog = useRef();
+const ResultModal = forwardRef(
+    ({ targetTime, remainingTime, onReset }, ref) => {
+        const dialog = useRef();
+        const userLost = remainingTime <= 0;
+        const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+        const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-    // exposing Component's API to make it more robust and reUsable
-    // 1. For this component, we are using showModal() outside this method.
-    // 2. So we have the need to expose a function that can be called with
-    // the help of a REF outside of this component, which will work independen
-    // on how this component would change in the future.
-    // 3. The useImperativeHandle hook is meant to work with fwRef hook
-    useImperativeHandle(ref, () => {
-        return {
-            open() {
-                dialog.current.showModal();
-            },
-        };
-    });
+        // exposing Component's API to make it more robust and reUsable
+        // 1. For this component, we are using showModal() outside this method.
+        // 2. So we have the need to expose a function that can be called with
+        // the help of a REF outside of this component, which will work independen
+        // on how this component would change in the future.
+        // 3. The useImperativeHandle hook is meant to work with fwRef hook
+        useImperativeHandle(ref, () => {
+            return {
+                open() {
+                    dialog.current.showModal();
+                },
+            };
+        });
 
-    return (
-        <dialog className="result-modal" ref={dialog}>
-            <h2>You {result} </h2>
-            <p>
-                The target time was{" "}
-                <strong>
-                    {targetTime} second{targetTime > 1 ? "s" : ""}.
-                </strong>
-            </p>
-            <p>
-                You stopped the timer wtih <strong>X seconds left.</strong>
-            </p>
-            <form method="dialog">
-                <button>Close</button>
-            </form>
-        </dialog>
-    );
-});
+        return (
+            <dialog className="result-modal" ref={dialog}>
+                <h2>You {userLost ? "lost" : "won, your score: " + score} </h2>
+                <p>
+                    The target time was{" "}
+                    <strong>
+                        {targetTime} second{targetTime > 1 ? "s" : ""}.
+                    </strong>
+                </p>
+                <p>
+                    You stopped the timer wtih{" "}
+                    <strong>{formattedRemainingTime} seconds left.</strong>
+                </p>
+                <form method="dialog" onSubmit={() => onReset}>
+                    <button>Close</button>
+                </form>
+            </dialog>
+        );
+    }
+);
 
 export default ResultModal;
--- ReactCG2024/05-refs-portals-start/src/components/TimerChallenge.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/TimerChallenge.jsx
@@ -4,25 +4,41 @@ import ResultModal from "./ResultModal";
 const TimerChallenge = ({ title, targetTime }) => {
     const timer = useRef();
     const dialog = useRef();
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive =
+        timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if (timeRemaining <= 0) {
+        // stop the timer programmatically
+        clearInterval(timer.current);
+        // if the timer expired, we need to show the modal wnd
+        dialog.current.open();
+    }
+
+    const handleReset = () => {
+        // reset the timer to the init state, doing this could be dangerous if we don't have a condition.
+        setTimeRemaining(targetTime * 1000);
+    };
 
     const handleStart = () => {
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.open();
-        }, targetTime * 1000);
-
-        setTimerStarted(true);
+        timer.current = setInterval(() => {
+            setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+        }, 10);
     };
 
     const handleStop = () => {
-        clearTimeout(timer.current);
+        dialog.current.open();
+        clearInterval(timer.current);
     };
 
     return (
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
@@ -31,16 +47,16 @@ const TimerChallenge = ({ title, targetTime }) => {
                 <p>
                     <button
                         onClick={
-                            timerStarted
+                            timerIsActive
                                 ? (e) => handleStop()
                                 : (e) => handleStart()
                         }
                     >
-                        {timerStarted ? "Stop" : "Start"} Challenge
+                        {timerIsActive ? "Stop" : "Start"} Challenge
                     </button>
                 </p>
-                <p className={timerStarted ? "active" : undefined}>
-                    {timerStarted ? "Time is running..." : "Timer inactive"}
+                <p className={timerIsActive ? "active" : undefined}>
+                    {timerIsActive ? "Time is running..." : "Timer inactive"}
                 </p>
             </section>
         </>
