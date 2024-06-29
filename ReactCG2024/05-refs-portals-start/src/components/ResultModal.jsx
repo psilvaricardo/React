da5d21ed05ab670fe7bf446fa85010dfe4bfb03f
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-29 14:04:55 -0500

    Expose the Open method via Component API to de-attach the dialog from the component

--- ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
+++ ReactCG2024/05-refs-portals-start/src/components/ResultModal.jsx
@@ -1,8 +1,24 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+    const dialog = useRef();
+
+    // exposing Component's API to make it more robust and reUsable
+    // 1. For this component, we are using showModal() outside this method.
+    // 2. So we have the need to expose a function that can be called with
+    // the help of a REF outside of this component, which will work independen
+    // on how this component would change in the future.
+    // 3. The useImperativeHandle hook is meant to work with fwRef hook
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            },
+        };
+    });
+
     return (
-        <dialog className="result-modal" ref={ref}>
+        <dialog className="result-modal" ref={dialog}>
             <h2>You {result} </h2>
             <p>
                 The target time was{" "}
