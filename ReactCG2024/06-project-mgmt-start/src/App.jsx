a66f9be96f1b4f208acdbb8cf13a1c4e3bc4314f
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-30 10:07:42 -0500

    Installing tailwind and fixing minor issues

--- ReactCG2024/06-project-mgmt-start/src/App.jsx
+++ ReactCG2024/06-project-mgmt-start/src/App.jsx
@@ -1,10 +1,9 @@
 const App = () => {
-    const [count, setCount] = useState(0);
-
-    return;
-    <>
-        <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>;
+    return (
+        <>
+            <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+        </>
+    );
 };
 
 export default App;
