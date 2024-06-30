1b784657d3dd441c0a0acd9dfbe73a978fa79ccf
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-06-30 17:43:29 -0500

    Styling buttons and Inputs with TailWind CSS

--- ReactCG2024/06-project-mgmt-start/src/components/Input.jsx
+++ ReactCG2024/06-project-mgmt-start/src/components/Input.jsx
@@ -1,8 +1,17 @@
 const Input = ({ label, textarea, ...props }) => {
+    const css_classes =
+        "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
     return (
-        <p>
-            <label>{label}</label>
-            {textarea ? <textarea {...props} /> : <input {...props} />}
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">
+                {label}
+            </label>
+            {textarea ? (
+                <textarea className={css_classes} {...props} />
+            ) : (
+                <input className={css_classes} {...props} />
+            )}
         </p>
     );
 };
