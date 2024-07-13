2f17ce9839d8efb911abdc757d84929f1f7c0a63
psilvaricardo <125488497+psilvaricardo@users.noreply.github.com>
2024-07-12 19:05:51 -0500

    Embracing component composition to get rid of one layer of prop drilling

--- ReactCG2024/07-adv-state-mgmt-context/src/App.jsx
+++ ReactCG2024/07-adv-state-mgmt-context/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 const App = () => {
@@ -72,7 +73,16 @@ const App = () => {
                 cart={shoppingCart}
                 onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
             />
-            <Shop onAddItemToCart={handleAddItemToCart} />
+            <Shop>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <li key={product.id}>
+                        <Product
+                            {...product}
+                            onAddToCart={handleAddItemToCart}
+                        />
+                    </li>
+                ))}
+            </Shop>
         </>
     );
 };
