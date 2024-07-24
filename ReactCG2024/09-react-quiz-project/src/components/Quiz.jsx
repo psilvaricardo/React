cab9c84b82c00b08503c24fb9ea4f58a4bb94618
psilvaricardo <125488497+psilvaricardo@users.noreply.github.com>
2024-07-24 18:54:23 -0500

    Adding state and deriving values from it

--- ReactCG2024/09-react-quiz-project/src/components/Quiz.jsx
+++ ReactCG2024/09-react-quiz-project/src/components/Quiz.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Quiz = () => {
+    // index-based logic to render the questions to the user.
+    // example: 2 answers given means 2 question answered
+    // and next question should be third, and index = 2
+    const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
     return (
         <>
             <p>Currently Active Question</p>
