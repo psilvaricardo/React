d24efabbf629daef11bd556236e53fb8e4db784c
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-08-03 12:37:03 -0500

    Enable MillionJS

--- ReactCG2024/10-react-behind-scenes/vite.config.js
+++ ReactCG2024/10-react-behind-scenes/vite.config.js
@@ -1,8 +1,7 @@
-import MillionLint from '@million/lint';
-import million from "million/compiler";
+import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 const _plugins = [MillionLint.vite()];
-_plugins.unshift(MillionLint.vite())
+_plugins.unshift(MillionLint.vite());
 export default defineConfig({
-  plugins: _plugins
+    plugins: _plugins,
 });
