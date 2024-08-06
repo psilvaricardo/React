8ec4fa2bbe2b3751013017df28ad6a3430e07023
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-08-05 20:30:21 -0500

    Handling loading states

--- ReactCG2024/11-sending-http-requests/src/components/AvailablePlaces.jsx
+++ ReactCG2024/11-sending-http-requests/src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 const AvailablePlaces = ({ onSelectPlace }) => {
+    const [isFetching, setIsFetching] = useState(false);
     const [availablePlaces, setAvailablePlaces] = useState([]);
 
     useEffect(() => {
         const fetchPlaces = async () => {
+            setIsFetching(true);
             const response = await fetch("http://localhost:3000/places");
             const data = await response.json();
             setAvailablePlaces(data.places);
+            setIsFetching(false);
         };
         fetchPlaces();
     }, []);
@@ -17,6 +20,8 @@ const AvailablePlaces = ({ onSelectPlace }) => {
         <Places
             title="Available Places"
             places={availablePlaces}
+            isLoading={isFetching}
+            loadingText="Fetching place data..."
             fallbackText="No places available."
             onSelectPlace={onSelectPlace}
         />
