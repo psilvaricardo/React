d1e83ef740ea9052b8b4faeedc553ee0dc59c3dd
psilvaricardo <125488497+psilvaricardo@users.noreply.github.com>
2024-08-27 15:30:11 -0500

    Handling Form via Refs

--- ReactCG2024/13-react-forms/src/App.jsx
+++ ReactCG2024/13-react-forms/src/App.jsx
@@ -2,7 +2,8 @@ import Header from "./components/Header";
 // import Login from "./components/Login";
 // import LoginSubmit from "./components/LoginSubmit";
 // import LoginState from "./components/LoginState";
-import LoginStateEnhanced from "./components/LoginStateEnhanced";
+// import LoginStateEnhanced from "./components/LoginStateEnhanced";
+import LoginRefs from "./components/LoginRefs";
 // import Signup from "./components/Signup";
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
         <>
             <Header />
             <main>
-                <LoginStateEnhanced />
+                <LoginRefs />
             </main>
         </>
     );
