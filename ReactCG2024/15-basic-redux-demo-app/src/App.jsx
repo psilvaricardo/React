cbe3247997d7195d662be45fd8fd5150b4b929cf
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-09-02 13:38:36 -0500

    Adding application content

--- ReactCG2024/15-basic-redux-demo-app/src/App.jsx
+++ ReactCG2024/15-basic-redux-demo-app/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
-export default App
+    return (
+        <Fragment>
+            <Header />
+            {!isAuth && <Auth />}
+            {isAuth && <UserProfile />}
+            <Counter />
+        </Fragment>
+    );
+};
+
+export default App;
