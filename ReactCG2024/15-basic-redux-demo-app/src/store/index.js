0f4c89639629b13461107a1af3611afc8c1a67f4
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-09-02 18:59:45 -0500

    Adding redux toolkit and making minor fixes

--- ReactCG2024/15-basic-redux-demo-app/src/store/index.js
+++ ReactCG2024/15-basic-redux-demo-app/src/store/index.js
@@ -4,7 +4,10 @@ import counterReducer from "./counter";
 import authReducer from "./auth";
 
 const store = configureStore({
-    reducer: { counter: counterReducer, auth: authReducer },
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer,
+    },
 });
 
 export default store;
