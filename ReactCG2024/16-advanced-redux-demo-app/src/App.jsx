ead5baecc39aecf20f9cb02bbb8dd9439c29a90b
psilvaricardo <125488497+psilvaricardo@users.noreply.github.com>
2024-09-04 12:49:07 -0500

    Fixing components to use functional syntaxis

--- ReactCG2024/16-advanced-redux-demo-app/src/App.jsx
+++ ReactCG2024/16-advanced-redux-demo-app/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+let isInitial = true;
 
-export default App
+const App = () => {
+    const dispatch = useDispatch();
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    const notification = useSelector((state) => state.ui.notification);
+
+    useEffect(() => {
+        dispatch(fetchCartData());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (isInitial) {
+            isInitial = false;
+            return;
+        }
+
+        if (cart.changed) {
+            dispatch(sendCartData(cart));
+        }
+    }, [cart, dispatch]);
+
+    return (
+        <Fragment>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
+    );
+};
+
+export default App;
--- ReactCG2024/16-advanced-redux-demo-app/src/components/Cart/CartButton.jsx
+++ ReactCG2024/16-advanced-redux-demo-app/src/components/Cart/CartButton.jsx
@@ -1,22 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import { uiActions } from '../../store/ui-slice';
-import classes from './CartButton.module.css';
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
-  const dispatch = useDispatch();
-  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+    const dispatch = useDispatch();
+    const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
-  const toggleCartHandler = () => {
-    dispatch(uiActions.toggle());
-  };
+    const toggleCartHandler = () => {
+        dispatch(uiActions.toggle());
+    };
 
-  return (
-    <button className={classes.button} onClick={toggleCartHandler}>
-      <span>My Cart</span>
-      <span className={classes.badge}>{cartQuantity}</span>
-    </button>
-  );
+    return (
+        <button className={classes.button} onClick={toggleCartHandler}>
+            <span>My Cart</span>
+            <span className={classes.badge}>{cartQuantity}</span>
+        </button>
+    );
 };
 
 export default CartButton;
