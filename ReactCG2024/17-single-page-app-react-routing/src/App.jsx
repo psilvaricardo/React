5fdc2735ab607496b05e22fbf60f6ffdd27c577e
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-10-22 19:21:42 -0500

    Adding super basic router just for demo

--- ReactCG2024/17-single-page-app-react-routing/src/App.jsx
+++ ReactCG2024/17-single-page-app-react-routing/src/App.jsx
@@ -1,5 +1,11 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+// adding route/path definition objects
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 const App = () => {
-    return <div></div>;
+    return <RouterProvider router={router} />;
 };
 
 export default App;
