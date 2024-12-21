204cef737aecb8723fec3969042b39e71d58c525
Ricardo Plascencia Silva <ricardo072@gmail.com>
2024-12-21 14:05:48 -0600

    Adding docs to the App component @ Routing

--- ReactCG2024/17-single-page-app-react-routing/src/App.jsx
+++ ReactCG2024/17-single-page-app-react-routing/src/App.jsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 
-// adding route/path definition objects
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+// we pass an array of route definition 
+// objects, each of them represent a route.
+// https://reactrouter.com/home 
+const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> }
+]);
 
 const App = () => {
     return <RouterProvider router={router} />;
